5ad4ea368beb1e040c1fd3898c2732db6873473d
artyomshpakovski <ashpakovskiy19@gmail.com>
2023-04-19 01:25:24 +0300

    modified

--- example3_gr14a_shpakovskiartyom.c
+++ example3_gr14a_shpakovskiartyom.c
@@ -1,17 +1,10 @@
 #include <sqlite3.h>
 #include <stdio.h>
-
-
 int callback(void *, int, char **, char **);
-
-
 int main(void) {
-    
     sqlite3 *db;
     char *err_msg = 0;
-    
     int rc = sqlite3_open("test.db", &db);
-    
     if (rc != SQLITE_OK) {
         
         fprintf(stderr, "Cannot open database: %s\n", 
@@ -20,11 +13,8 @@ int main(void) {
         
         return 1;
     }
-    
     char *sql = "SELECT * FROM Cars";
-        
     rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
-    
     if (rc != SQLITE_OK ) {
         
         fprintf(stderr, "Failed to select data\n");
@@ -35,23 +25,16 @@ int main(void) {
         
         return 1;
     } 
-    
     sqlite3_close(db);
-    
     return 0;
 }
 
 int callback(void *NotUsed, int argc, char **argv, 
                     char **azColName) {
-    
     NotUsed = 0;
-    
     for (int i = 0; i < argc; i++) {
-
         printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
     }
-    
     printf("\n");
-    
     return 0;
 }
